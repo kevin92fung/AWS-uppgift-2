2eb2831ccead5fe83b072d4c2e8ba3d339a9eeb2
kevin <test@test.se>
2024-11-03 15:31:40 +0100

    update deploy parameters value

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+#change the value of the parameters for your wp env.
 aws cloudformation deploy \
   --template-file CloudFormation.yaml \
   --stack-name uppgift2 \
   --parameter-overrides \
     AdminIP=0.0.0.0/0 \
     SSHKey=ssh \
-    MasterUsername=rdsusername \
-    MasterUserPassword=kodtillrds \
-    DBName=wordpressdbnamn \
-    WPTitle=wordpresstitel \
-    WPAdminUser=wordpressuser \
-    WPAdminPassword=wordpresskod \
-    WPAdminEmail=admin@mail.se
+    MasterUsername=admin \
+    MasterUserPassword=Password123. \
+    DBName=wordpressdb \
+    WPTitle=Awsesome \
+    WPAdminUser=admin \
+    WPAdminPassword=password \
+    WPAdminEmail=sample@mail.com
